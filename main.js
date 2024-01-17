5a2fc5ac9458a99adbe2deb1b639d4a3a497b5f2
unknown <tmcfall.prsvr@gmail.com>
2024-01-17 09:35:55 -0500

    added

--- main.js
+++ main.js
@@ -2,9 +2,20 @@ let paragraph = document.querySelector('p')
 paragraph.style.color = "aqua"
 paragraph.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id urna a libero interdum posuere. Donec vitae aliquet sapien. Vivamus sagittis erat a lacus tincidunt, vel feugiat odio ultrices."
 
-let heading = document.querySelector('heading')
+let heading = document.querySelector('h1')
 heading.style.font = '42px'
 
 let listItem = document.querySelectorAll('li')
 listItem[12].style.opacity = "30%"
-listItem[2].innerText = "Greetings!"
+listItem[2].innerHTML = "Greetings!"
+
+
+let image = document.querySelector('img')
+image.src = "https://media.istockphoto.com/id/1470130937/photo/young-plants-growing-in-a-crack-on-a-concrete-footpath-conquering-adversity-concept.webp?b=1&s=170667a&w=0&k=20&c=IRaA17rmaWOJkmjU_KD29jZo4E6ZtG0niRpIXQN17fc="
+image.style.height = '250px'
+
+
+image.src.innerHTML = "https://tse4.mm.bing.net/th?id=OIP.WbuoeOSKLmHN55FeVkX2bAHaHa&pid=Api&P=0&h=220"
+
+
+listItem[16].innerHTML = "Never gonna give you up."
